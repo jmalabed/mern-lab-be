19335172f2cc403626e742a225d63763b238a5bc
jmalabed <jmalabed27@gmail.com>
2021-09-29 16:19:25 -0700

    model created

---
+++ models/product.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    default: "No description yet."
+  },
+  reviews: [String],
+  image: String
+})
+
+module.exports = mongoose.model('Product', productSchema)
